7abb3601fc16c1e4024844f30fbda1d827282716
Ashwin K N <ashwinkn2004@gmail.com>
2024-08-26 19:24:36 +0530

    update

--- employee_payroll/lib/pay_roll.dart
+++ employee_payroll/lib/pay_roll.dart
@@ -17,156 +17,159 @@ class PayRoll extends StatelessWidget {
         ),
       ),
       body: const SingleChildScrollView(
-        child:  Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding:  EdgeInsets.all(8.0),
-              child: Text(
-                'Emploee Name: Dummy Data',
-                style: TextStyle(
-                  fontSize: 16,
+        child:  Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding:  EdgeInsets.all(8.0),
+                child: Text(
+                  'Emploee Name: Dummy Data',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
                 ),
               ),
-            ),
-            Divider(),
-        
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text(
-                'Emploee ID: Dummy Data',
-                style: TextStyle(
-                  fontSize: 16,
+              Divider(),
+          
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Emploee ID: Dummy Data',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
                 ),
               ),
-            ),
-            Divider(),
-        
-            Padding(
-              padding:  EdgeInsets.all(8.0),
-              child: Text(
-                'Emploee Department: Dummy Data',
-                style: TextStyle(
-                  fontSize: 16,
+              Divider(),
+          
+              Padding(
+                padding:  EdgeInsets.all(8.0),
+                child: Text(
+                  'Emploee Department: Dummy Data',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
                 ),
               ),
-            ),
-            Divider(),
-        
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text(
-                'Emploee Email: Dummy Data',
-                style: TextStyle(
-                  fontSize: 16,
+              Divider(),
+          
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Emploee Email: Dummy Data',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
                 ),
               ),
-            ),
-            Divider(),
-        
-            Padding(
-              padding:  EdgeInsets.all(8.0),
-              child: Text(
-                'Emploee Gender: Dummy Data',
-                style: TextStyle(
-                  fontSize: 16,
+              Divider(),
+          
+              Padding(
+                padding:  EdgeInsets.all(8.0),
+                child: Text(
+                  'Emploee Gender: Dummy Data',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
                 ),
               ),
-            ),
-            Divider(),
-        
-            Padding(
-              padding:  EdgeInsets.all(8.0),
-              child: Text(
-                'Emploee DOB: Dummy Data',
-                style: TextStyle(
-                  fontSize: 16,
+              Divider(),
+          
+              Padding(
+                padding:  EdgeInsets.all(8.0),
+                child: Text(
+                  'Emploee DOB: Dummy Data',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
                 ),
               ),
-            ),
-            Divider(),
-        
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text(
-                'Emploee Mobile: Dummy Data',
-                style: TextStyle(
-                  fontSize: 16,
+              Divider(),
+          
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Emploee Mobile: Dummy Data',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
                 ),
               ),
-            ),
-            Divider(),
-        
-            Padding(
-              padding:  EdgeInsets.all(8.0),
-              child: Text(
-                'Emploee Address: Dummy Data',
-                style: TextStyle(
-                  fontSize: 16,
+              Divider(),
+          
+              Padding(
+                padding:  EdgeInsets.all(8.0),
+                child: Text(
+                  'Emploee Address: Dummy Data',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
                 ),
               ),
-            ),
-            Divider(),
-        
-            Padding(
-              padding:  EdgeInsets.all(8.0),
-              child: Text(
-                'Basic Pay: Dummy Data',
-                style: TextStyle(
-                  fontSize: 16,
+              Divider(),
+          
+              Padding(
+                padding:  EdgeInsets.all(8.0),
+                child: Text(
+                  'Basic Pay: Dummy Data',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
                 ),
               ),
-            ),
-            Divider(),
-        
-            Padding(
-              padding:  EdgeInsets.all(8.0),
-              child: Text(
-                'DA: Dummy Data',
-                style: TextStyle(
-                  fontSize: 16,
+              Divider(),
+          
+              Padding(
+                padding:  EdgeInsets.all(8.0),
+                child: Text(
+                  'DA: Dummy Data',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
                 ),
               ),
-            ),
-            Divider(),
-        
-            Padding(
-              padding:  EdgeInsets.all(8.0),
-              child: Text(
-                'HRA',
-                style: TextStyle(
-                  fontSize: 16,
+              Divider(),
+          
+              Padding(
+                padding:  EdgeInsets.all(8.0),
+                child: Text(
+                  'HRA',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
                 ),
               ),
-            ),
-            Divider(),
-        
-            Padding(
-              padding:  EdgeInsets.all(8.0),
-              child: Text(
-                'TA',
-                style: TextStyle(
-                  fontSize: 16,
+              Divider(),
+          
+              Padding(
+                padding:  EdgeInsets.all(8.0),
+                child: Text(
+                  'TA',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
                 ),
               ),
-            ),
-            Divider(),
-
-            SizedBox(
-              height: 20,
-            ),
-            Center(
-              child: Text(
-                'Net Salary 86000',
-                style: TextStyle(
-                  color: Colors.red,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
-              
+              Divider(),
+          
+              SizedBox(
+                height: 20,
               ),
-            )
-          ],
+              Center(
+                child: Text(
+                  'Net Salary 86000',
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
