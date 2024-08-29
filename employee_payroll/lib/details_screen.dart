c0e17a86028644508f243b06721c47450d3ca5ab
Ashwin K N <ashwinkn2004@gmail.com>
2024-08-29 17:35:39 +0530

    some changes made

--- employee_payroll/lib/details_screen.dart
+++ employee_payroll/lib/details_screen.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:employee_payroll/pay_roll.dart';
 
-class DetailsScreen extends StatelessWidget {
-  const DetailsScreen({super.key});
+class DetailsScreen extends StatefulWidget {
+  DetailsScreen({super.key});
+
+  @override
+  State<DetailsScreen> createState() => _DetailsScreenState();
+}
+
+class _DetailsScreenState extends State<DetailsScreen> {
+  List<String> department = ['CIVIL', 'CSE', 'AI', 'ECE', 'EEE', 'MECH', 'MCA'];
+  String? selectDepartment;
+
+  List<String> gender = ['MALE', 'FEMALE'];
+  String? selectGender;
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final nameController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +32,7 @@ class DetailsScreen extends StatelessWidget {
         ),
         actions: [
           IconButton(
-            onPressed: (){}, 
+            onPressed: () {},
             icon: const Icon(
               Icons.logout,
               color: Colors.white,
@@ -27,139 +41,169 @@ class DetailsScreen extends StatelessWidget {
         ],
       ),
       body: SingleChildScrollView(
-        child: Column(children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Employee Name',
-                hintStyle: TextStyle(
-                  color: Colors.grey[500],
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10)
-                )
-              ),
-            ),
-          ),
-        
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Employee ID',
-                hintStyle: TextStyle(
-                  color: Colors.grey[500],
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10)
-                )
-              ),
-            ),
-          ),
-        
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Department',
-                hintStyle: TextStyle(
-                  color: Colors.grey[500],
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10)
-                )
-              ),
-            ),
-          ),
-        
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Email',
-                hintStyle: TextStyle(
-                  color: Colors.grey[500],
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10)
-                )
-              ),
-            ),
-          ),
-        
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Gender',
-                hintStyle: TextStyle(
-                  color: Colors.grey[500],
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10)
-                )
-              ),
-            ),
-          ),
-        
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Mobile Number',
-                hintStyle: TextStyle(
-                  color: Colors.grey[500],
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10)
-                )
-              ),
-            ),
-          ),
-        
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Address',
-                hintStyle: TextStyle(
-                  color: Colors.grey[500],
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10)
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: nameController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Name Cannot be empty!';
+                    } else {
+                      return null;
+                    }
+                  },
+                  decoration: InputDecoration(
+                      hintText: 'Employee Name',
+                      hintStyle: TextStyle(
+                        color: Colors.grey[500],
+                      ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10))),
                 ),
               ),
-              maxLines: 5,
-            ),
-          ),
-        
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Basic Pay',
-                hintStyle: TextStyle(
-                  color: Colors.grey[500],
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintText: 'Employee ID',
+                      hintStyle: TextStyle(
+                        color: Colors.grey[500],
+                      ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10))),
                 ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10)
-                )
               ),
-            ),
-          ),
 
-          ElevatedButton(onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context) => const PayRoll()));
-          },
-            style: ButtonStyle(
-              backgroundColor: MaterialStatePropertyAll(Colors.grey[800]),
-            ), child: const Text(
-            'Process Roll',
-            style: TextStyle(color: Colors.white),
-            ),
+              /*Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintText: 'Department',
+                      hintStyle: TextStyle(
+                        color: Colors.grey[500],
+                      ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                ),
+              ),*/
+
+              DropdownButton(
+                value: selectDepartment,
+                hint: Text('Select Department'),
+                items: department.map<DropdownMenuItem<String>>(
+                  (String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  },
+                ).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    selectDepartment = value;
+                  });
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintText: 'Email',
+                      hintStyle: TextStyle(
+                        color: Colors.grey[500],
+                      ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                ),
+              ),
+              /*Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintText: 'Gender',
+                      hintStyle: TextStyle(
+                        color: Colors.grey[500],
+                      ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                ),
+              ),*/
+
+              DropdownButton(
+                value: selectGender,
+                hint: Text('Select Gender'),
+                items: gender.map<DropdownMenuItem<String>>(
+                  (String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  },
+                ).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    selectDepartment = value;
+                  });
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  keyboardType: TextInputType.numberWithOptions(),
+                  decoration: InputDecoration(
+                      hintText: 'Mobile Number',
+                      hintStyle: TextStyle(
+                        color: Colors.grey[500],
+                      ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Address',
+                    hintStyle: TextStyle(
+                      color: Colors.grey[500],
+                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                  maxLines: 5,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintText: 'Basic Pay',
+                      hintStyle: TextStyle(
+                        color: Colors.grey[500],
+                      ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const PayRoll()));
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Colors.grey[800]),
+                ),
+                child: const Text(
+                  'Process Roll',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
           ),
-        ],
         ),
       ),
     );
